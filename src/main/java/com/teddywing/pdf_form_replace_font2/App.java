77ab2798d17023e92347c5b2ea1e2d56410111fa
Teddy Wing <git@teddywing.com>
2023-04-23 20:15:40 +0200

    App.java: Remove commented in-progress code
    Now that we have something that works, remove the old code from when I
    was testing things out.

--- src/main/java/com/teddywing/pdf_form_replace_font2/App.java
+++ src/main/java/com/teddywing/pdf_form_replace_font2/App.java
@@ -20,39 +20,27 @@ public class App {
 			new PdfWriter("f1040-courier.pdf")
 		);
 
-		// FontProgramFactory.registerSystemFontDirectories();
-		// FontProgramFactory.registerFontDirectory("/System/Library/Fonts/");
-		FontProgramFactory.registerFontDirectory("/System/Library/Fonts/Supplemental/");
-		// FontProgramFactory
-		// 	.registerFont("/System/Library/Fonts/Supplemental/Courier New.ttf", "hihoaliaso");
-		// System.out.println("is registered: "
-		// 	+ FontProgramFactory.isRegisteredFont("CourierNewPSMT"));
+		FontProgramFactory
+			.registerFontDirectory("/System/Library/Fonts/Supplemental/");
 
 		System.out.println(FontProgramFactory.getRegisteredFonts());
 		System.out.println(FontProgramFactory.getRegisteredFontFamilies());
 
-		// FontProgram courier_program = FontProgramFactory.createFont("CourierNewPSMT");
-		// FontProgram courier_program = FontProgramFactory
-		// 	.createFont("/System/Library/Fonts/Supplemental/Courier New.ttf");
 		FontProgram courier_program = FontProgramFactory
 			.createRegisteredFont("CourierNewPSMT");
-		// FontProgram courier_program = FontProgramFactory
-		// 	.createFont("Courier New.ttf");
-		// FontProgram courier_program = FontProgramFactory
-		// 	.createFont("/System/Library/Fonts/Supplemental/Copperplate.ttc", 0, false);
-		PdfFont courier = PdfFontFactory
-			.createFont(courier_program, PdfEncodings.UTF8, PdfFontFactory.EmbeddingStrategy.FORCE_NOT_EMBEDDED);
+		PdfFont courier = PdfFontFactory.createFont(
+			courier_program,
+			PdfEncodings.UTF8,
+			PdfFontFactory.EmbeddingStrategy.FORCE_NOT_EMBEDDED
+		);
 
 		PdfAcroForm form = PdfAcroForm.getAcroForm(pdf, false);
 		Map<String, PdfFormField> fields = form.getFormFields();
 
 		for (var entry : fields.entrySet()) {
-			// TODO: Try printing original font family and size on field.
 			PdfFormField field = entry.getValue();
 
 			PdfFont original_font = field.getFont();
-			// System.out.println("Font: " + field.getFontSize());
-
 			String original_postscript_name = original_font
 				.getFontProgram()
 				.getFontNames()
@@ -60,8 +48,6 @@ public class App {
 
 			if (original_postscript_name.equals("HelveticaLTStd-Bold")) {
 				field.setFont(courier);
-				// System.out.println("F: " + field.getFont());
-				// System.out.println("R: " + field.regenerateField());
 			}
 		}
 
